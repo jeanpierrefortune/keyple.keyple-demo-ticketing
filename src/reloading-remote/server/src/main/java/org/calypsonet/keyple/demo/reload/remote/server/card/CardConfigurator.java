8a50a5be42e14a47094c04761d17457cbdb08e5f
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-07-24 16:25:56 +0200

    docs: update license to BSD 3-Clause and adjust paths

--- src/reloading-remote/server/src/main/java/org/calypsonet/keyple/demo/reload/remote/server/card/CardConfigurator.java
+++ src/reloading-remote/server/src/main/java/org/calypsonet/keyple/demo/reload/remote/server/card/CardConfigurator.java
@@ -1,11 +1,14 @@
 /* ******************************************************************************
  * Copyright (c) 2020 Calypso Networks Association https://calypsonet.org/
  *
- * This program and the accompanying materials are made available under the
- * terms of the MIT License which is available at
- * https://opensource.org/licenses/MIT.
+ * See the NOTICE file(s) distributed with this work for additional information
+ * regarding copyright ownership.
  *
- * SPDX-License-Identifier: MIT
+ * This program and the accompanying materials are made available under the
+ * terms of the BSD 3-Clause License which is available at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
  ****************************************************************************** */
 package org.calypsonet.keyple.demo.reload.remote.server.card;
 
